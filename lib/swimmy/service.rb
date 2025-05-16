d8ecbd6a5c8715b191b2470691ede870f955ab80
Kazuyuki2025 <pbww1g8g@s.okayama-u.ac.jp>
2025-05-16 15:26:46 +0900

    Modify karaoke_cmd

--- lib/swimmy/service.rb
+++ lib/swimmy/service.rb
@@ -18,5 +18,6 @@ module Swimmy
     autoload :Coop, "#{dir}/coop.rb"
     autoload :Nomnichi, "#{dir}/nomnichi.rb"
     autoload :RecipeInfomation, "#{dir}/recipe_information.rb"
+    autoload :Karaoke, "#{dir}/karaoke.rb"
   end
 end
