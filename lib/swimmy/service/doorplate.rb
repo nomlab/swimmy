e55fffeca4ed22580e6416de7570d02c0896453d
fujiwara-e <pjxk6clf@s.okayama-u.ac.jp>
2026-07-23 09:53:43 +0900

    Add comment

--- lib/swimmy/service/doorplate.rb
+++ lib/swimmy/service/doorplate.rb
@@ -10,6 +10,9 @@ module Swimmy
 
         topic = "cmd/pinot/v1/ou/eng4/doormgr/state"
         payload = JSON.dump({
+          # Slack の user_name と door-mgr の member は対応している．
+          # しかし door-mgr では`-`が利用できず`_`に置換したものを期待しているため変換する．
+          # e.g. user_name: "fujiwara-e" -> member: "fujiwara_e"
           member: user_name.tr('-','_'),
           position: attendance,
         })
