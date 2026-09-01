848bd51e9f836bfe66dc6001434032bacd1f5407
Yoshinari Nomura <nom@nomlab.org>
2026-09-01 17:39:46 +0900

    Nomnichi command to adopt new Hugo site

--- lib/swimmy/resource/nomnichi_article.rb
+++ lib/swimmy/resource/nomnichi_article.rb
@@ -15,23 +15,29 @@ module Swimmy
             hash["approved"],
             hash["count"],
             hash["promote_headline"],
-            hash["url"])
+            hash["url"],
+            hash["user_name"]
+          )
         end
   
         def initialize(id, user_id, title, perma_link, content,
-                       published_on, approved, count, promote_headline, url)
+                       published_on, approved, count, promote_headline, url, user_name = nil)
   
           @id, @user_id, @title, @perma_link, @content,
           @published_on, @approved, @count, @promote_headline,
-          @url =id, user_id, title, perma_link, content,
-          published_on, approved, count, promote_headline, url
+          @url, @user_name =id, user_id, title, perma_link, content,
+          published_on, approved, count, promote_headline, url, user_name
         end
   
+        def user_name
+          @user_name ||= user_name_from_permalink
+        end
+
         # Since we have no way to aquire user_name from JSON,
         # get it from `perma_link`:
         #   "yoshida-s-20181116-132016" => yoshida-s
         #
-        def user_name
+        def user_name_from_permalink
           if /(.*)-\d{8}-\d{6}$/ =~ perma_link
             return $1
           else
--- lib/swimmy/service/nomnichi.rb
+++ lib/swimmy/service/nomnichi.rb
@@ -1,7 +1,33 @@
 module Swimmy
     module Service
       class Nomnichi
-        def fetch(max_page = 10)
+      def fetch()
+        require "net/http"
+        base_url = "https://gc.cs.okayama-u.ac.jp/lab/nom/articles/index.xml"
+
+        uri = URI.parse(base_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE # SSL証明書を無視
+        req = Net::HTTP::Get.new(uri.path)
+        res = http.request(req)
+
+        return [] if res.code != "200" # failed to fetch
+        doc = Nokogiri::XML::parse(res.body)
+
+        doc.xpath('//item').map {|item|
+          hash = {
+            'user_name' => item.at('author').text,
+            'title' => item.at('title').text,
+            'perma_link' => item.at('link').text,
+            'published_on' => item.at('pubDate').text,
+            'content' => item.at('description').text,
+          }
+          Swimmy::Resource::NomnichiArticle.parse_hash(hash)
+        }
+      end
+
+        def fetch_old(max_page = 10)
           require "json"
           require "net/http"
           base_url = "https://gc.cs.okayama-u.ac.jp/lab/nom/articles.json?page="
